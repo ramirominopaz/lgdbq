fcc98c8f9f2ef88b134f1e1c50783faa8e575c1e
Ramiro Miño Paz <minopazramiro@gmail.com>
2023-11-24 01:55:27 -0300

    inicial-commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1 align="center"> Su título aquí </h1>
+<span><img href="media/icono.png"></span>
+
+<h2>¿Que es La Guía del Buen Químico?</h2>
+
+<p>"La Guía del Buen Químico" fue un proyecto que desarrolle junto con algunos compañeros durante mi ultimo año de la escuela secundaria para la Feria de Ciencias de INNOVA</p><br>
+
+<p>Este proyecto consta de un bot de telegram cuya finalidad es acercar los conocimientos de la química de una forma mas interactiva (y que ya se encuentra publicado en mi perfil de github) y esta pagina web que busca reforzar los conocimientos de una manera más teorica.</p><br>
+
+<h2 color=red>Disclaimer:</h2>
+<p>Debido a que la página no se encuentra en su versión final no es recomendable utilizarla con fines educativos.</p><br>
+<p>Actualmente sigo avanzando en el desarrollo de la misma</p>
---
+++ pages/Quimica Inorganica/Oxigeno/oxigeno.js
@@ -0,0 +1,88 @@
+//Esto sirve para que se despliegue el menu lateral cuando la pantalla toma menores dimensiones
+
+document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+
+    document.querySelector(".menu").classList.toggle("mostrar_menu");
+}
+
+
+//Esto es una mini animacion pedorra de los iconos del banner uwu
+
+window.onscroll = function () {
+    var posicion = window.pageYOffset || document.documentElement.scrollTop;
+    var elemento1 = document.getElementById("icon_heart");
+    var elemento2 = document.getElementById("icon_fire");
+    elemento1.style.bottom = posicion * 0.23 + "px";
+    elemento2.style.top = posicion * 0.5 + "px";
+}
+
+
+document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
+
+
+// variables
+bars_search = document.getElementById("ctn-bars-search");
+cover_ctn_search = document.getElementById("cover-ctn-search");
+inputSearch = document.getElementById("inputSearch");
+box_search = document.getElementById("box-search");
+
+
+function mostrar_buscador(){
+    bars_search.style.top = "100px";
+    cover_ctn_search.style.display = "block";
+    inputSearch.focus();
+    if (inputSearch.value === ""){
+        box_search.style.display = "none";
+    }
+    
+}
+
+
+//para ocultar el buscador
+
+function ocultar_buscador(){
+    bars_search.style.top = "-10px";
+    cover_ctn_search.style.display = "none";
+    inputSearch.value = "";
+    box_search.style.display = "none";
+
+
+
+}
+
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+
+
+function buscador_interno(){
+
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    for (i = 0; i < li.length; i++){
+
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+
+            li[i].style.display = "";
+            box_search.style.display = "block";
+
+            if (inputSearch.value === ""){
+                box_search.style.display = "none";
+            }
+
+        }
+        else{
+            li[i].style.display = "none";
+        }
+
+    }
+
+
+
+}
